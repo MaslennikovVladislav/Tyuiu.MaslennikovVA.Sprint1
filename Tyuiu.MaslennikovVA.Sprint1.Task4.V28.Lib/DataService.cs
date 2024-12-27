964433959a6d39650c78aab6deaa6c85f06a9f3d
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 18:59:53 +0500

    Task4

--- Tyuiu.MaslennikovVA.Sprint1.Task4.V28.Lib/DataService.cs
+++ Tyuiu.MaslennikovVA.Sprint1.Task4.V28.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.MaslennikovVA.Sprint1.Task4.V28.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = Math.Round(Math.Cos(60 * 3.14 / 2) / Math.Exp(2 * x + y), 4);
+            var res = Math.Round(Math.Cos(60 * 3.1415 / 2) / Math.Exp(2 * x + y), 3);
             return res;
         }
     }
