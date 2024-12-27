0274f5f80faea9f3732674f73f98338e5b4db1ce
MaslennikovVladislav <Tyuiu.MaslennikovVA@outlook.com>
2024-12-27 18:06:53 +0500

    Task2

---
+++ Tyuiu.MaslennikovVA.Sprint1.Task2.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.MaslennikovVA.Sprint1.Task2.V28.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint1.Task2.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 0;
+            var res = ds.ConvertCelsiusToKelvin(x);
+            Assert.AreEqual(-237, res);
+        }
+    }
+}
---
+++ Tyuiu.MaslennikovVA.Sprint1.Task2.V28/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MaslennikovVA.Sprint1.Task2.V28.Lib;
+
+namespace Tyuiu.MaslennikovVA.Sprint1.Task2.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Масленников В.А. | ИИПб-24-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Арифметические операторы в C#                                           *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Масленников В.А. | ИИПб-24-2                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Известна температура в градусах Цельсия. Перевести температуру в        *");
+            Console.WriteLine("* градусы Кельвина.                                                       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("РЕЗУЛЬТАТ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("X в градусах Кельвина = " + ds.ConvertCelsiusToKelvin(x));
+
+            Console.ReadLine();
+        }
+    }
+}
